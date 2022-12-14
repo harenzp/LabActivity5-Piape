fdd172bcdf5b6910a42d4ec308c837ad03a582c5
Christian Rey Piape <harenzp14@gmail.com>
2022-12-15 03:10:50 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -3,6 +3,8 @@ package SimpleCalc;
 import LeapYear.LeapYearGUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -11,6 +13,34 @@ public class SimpleCalcGUI extends JFrame{
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+                    int n1 = Integer.parseInt(tfNumber1.getText());
+                    int n2 = Integer.parseInt(tfNumber2.getText());
+
+                    if (cbOperations.getSelectedItem().equals("+")){
+                        int result = n1+n2;
+                        lblResult.setText(String.valueOf(result));
+                    } else if (cbOperations.getSelectedItem().equals("-")){
+                        int result = n1-n2;
+                        lblResult.setText(String.valueOf(result));
+                    } else if (cbOperations.getSelectedItem().equals("*")){
+                        int result = n1*n2;
+                        lblResult.setText(String.valueOf(result));
+                    } else if (cbOperations.getSelectedItem().equals("/")){
+                        int result = n1/n2;
+                        lblResult.setText(String.valueOf(result));
+                    }
+
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
